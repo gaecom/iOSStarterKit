5e380bc59d12848f0d806492ef5bcf5f31122800
liuqin.sheng <sliuqin@gmail.com>
2013-11-08 20:14:51 +0800

    增加了对 循环滚动的说明.

---
+++ PageControlNavigation/PageControlNavigationTests/PageControlNavigationTests.m
@@ -0,0 +1,34 @@
+//
+//  PageControlNavigationTests.m
+//  PageControlNavigationTests
+//
+//  Created by liuqin.sheng on 11/8/13.
+//  Copyright (c) 2013 alipay. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PageControlNavigationTests : XCTestCase
+
+@end
+
+@implementation PageControlNavigationTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
